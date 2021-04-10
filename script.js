1d2baed4f4fb799c2c9ad51ffdec5f3532c9c32d
Frances Tang <francest221@gmail.com>
2021-04-10 14:22:27 -0700

    Replace with updated files

--- script.js
+++ script.js
@@ -1,50 +1,21 @@
-// Select a single order, display the price
-const latte = document.getElementById('latte')
-const americano = document.getElementById('americano')
-const macchiato = document.getElementById('macchiato')
-const cappuccino = document.getElementById('cappuccino')
-const frappuccino = document.getElementById('frappuccino')
+// Auto-populate form fields based on api 
 
-let drinks = [latte, americano, macchiato, cappuccino, frappuccino];
+document.addEventListener('DOMContentLoaded', function () {
 
-let prices = new Map();
-prices.set('Latte', '3.65')
-prices.set('Americano', '2.65')
-prices.set('Macchiato', '4.35')
-prices.set('Cappuccino', '5.25')
-prices.set('Frappuccino', '5.65')
-
-const savings = document.getElementById('savings');
-let p1 = document.createElement('p')
-savings.appendChild(p1)
-// adding a line break each time we add text content to p2
-p1.setAttribute('style', 'white-space: pre;')
-
-// loop over the array of drinks 
-// if one of the drinks gets clicked
-// show the price
-var clicked = false;
-sb_price = 0;
-drinks.forEach(function(item){
-    item.onclick = function() {
-        var coffee = item.innerHTML
-        clicked = true;
-        // clear the last requested information
-        p1.innerHTML = '';
-        p2.innerHTML = '';
-        // show the requested drink's price
-        sb_price = prices.get(coffee);
-        p1.textContent = 'Your go-to drink is a ' + coffee + '. ' + 'YUM! \r\n'
-        p1.textContent += 'The price of a ' + coffee + ' is $'
-        p1.textContent += sb_price;
-        p1.textContent += '. '
-
-        // add styling to new html conten
-        let newStyle = document.querySelector('#savings');
-        newStyle.classList.add("output");
-    }
-});
-
+    fetch('https://my-coffee-api.herokuapp.com/coffee')
+    .then((res) => res.text())
+    .then((data) => { 
+        data = JSON.parse(data);
+        let output = '';
+        
+        // create button for each coffee option in API
+        data.forEach(function(coffees) {
+            output += `<option class="dropdown-item">${coffees.name}</option>\r\n`;            
+        });
+        document.getElementById('dropdown-options').innerHTML += output;
+    }) 
+    .catch((err) => console.log(err));
+})
 
 // calculate compound interest 
 function calcTotalValue(year, daily_invest){
@@ -59,46 +30,100 @@ function calcTotalValue(year, daily_invest){
     return total_value.toFixed(2);
 }
 
-// show the potentional savings
+// creating elements for outputs
+const coffee_price = document.getElementById('coffee-price');
+let p1 = document.createElement('p')
+coffee_price.appendChild(p1)
 
+const daily_cost = document.getElementById('daily-cost');
 let p2 = document.createElement('p')
-savings.appendChild(p2)
+daily_cost.appendChild(p2)
 
-// adding a line break each time we add text content to p2
+const savings = document.getElementById('savings');
+let p3 = document.createElement('p')
+savings.appendChild(p3)
+
+p1.setAttribute('style', 'white-space: pre;')
 p2.setAttribute('style', 'white-space: pre;')
-let show = document.getElementById('button-enter');
+p3.setAttribute('style', 'white-space: pre;')
 
-// when the user clicks on the "Show me" button
-show.onclick = function () {
-    if (clicked == false) {
+// Take user input, output total savings
+
+document.getElementById('button-enter').onclick = function getSavings() {
+  
+    p1.innerHTML = '';
+    p2.innerHTML = '';
+    p3.innerHTML = '';
+
+    // populate coffee choice and quantity based on user input
+    let selection = document.getElementById('dropdown-options');
+    let coffee_choice = selection.options[selection.selectedIndex].text;
+    let qty = 0;
+    qty = document.getElementById('input-qty').value;
+
+    // if coffee or amount empty, re-prompt user to choose drink first ---set placeholder and then value == 0 for this to work
+    if (coffee_choice == "Choose your coffee order" || qty == 0) { 
         // erase the last requested information
         savings.innerHTML = '';
-        alert('Please choose a drink first. ');
+        alert('Please choose a drink & input-qty first. ');
         location.reload();
-    } 
+    }
+
     else {
-        p2.innerHTML = '';
-        p2.textContent += 'If you make coffee at home instead: \r\n'
-        p2.textContent += 'Ground Coffee: $12 / 80 cups \r\n'
-        p2.textContent += 'Filters: $0.02 \r\n'
-        p2.textContent += 'Creamer: $0.03 \r\n'
-        p2.textContent += 'Total= $0.20 per cup \r\n'
-        p2.textContent += '\r\n'
-        // calculate the savings per cup
-        const savings_per_cup = (sb_price - 0.20).toFixed(2);
-        p2.textContent += 'Having your coffee at home would save you $' + savings_per_cup + ' per cup! \r\n'
-        const daily_savings = (3*savings_per_cup).toFixed(2)
-        p2.textContent += '$' + savings_per_cup + ' per cup X 3 cups per day = $' + daily_savings + ' saved per day. \r\n'
-        p2.textContent += '\r\nIf you instead invested that $' + daily_savings + ' per day:\r\n'
-        var total_val = addCommas(calcTotalValue(55, parseInt(daily_savings)));
-        p2.textContent += 'After 55 years at a 7% return, \r\n'
-        p2.textContent += 'you would have $' + total_val + '!\r\n'
-     }
-    // reset the dropdown menu attribute
-    clicked = false;
+        let sb_price = 0;
+        let coffee_name = "";
+
+        fetch('https://my-coffee-api.herokuapp.com/coffee')
+        .then((res) => res.text())
+        .then((data) => { 
+            let coffees = JSON.parse(data);
+
+            coffees.forEach(function(coffees) {
+                if (coffees.name == coffee_choice) {
+                    coffee_name = coffees.name;
+                    sb_price = coffees.price; 
+                    console.log(sb_price);
+
+                    // show the requested drink's prices
+                    p1.textContent = 'The price of a grande ' + coffee_name + ' from Starbucks is ~$'
+                    p1.textContent += sb_price;
+                    p1.textContent += '. '
+
+                    // show daily savings
+                    var daily_cost = (sb_price * qty).toFixed(2);
+                    p2.textContent = 'Your coffee habit is costing you $' + daily_cost + ' per day! \r\n'
+                
+            
+                    // add styling to new html content
+                    let newStyle = document.querySelector('#results');
+                    newStyle.classList.add("output");
+
+                    // show total potential investment 
+                    p3.innerHTML = '';
+                    p3.textContent += 'If you make coffee at home instead: \r\n'
+                    p3.textContent += 'Ground Coffee: $12 / 80 cups \r\n'
+                    p3.textContent += 'Filters: $0.02 \r\n'
+                    p3.textContent += 'Creamer: $0.03 \r\n'
+                    p3.textContent += 'Total= $0.20 per cup \r\n'
+                    p3.textContent += '\r\n'
+
+                    // calculate the savings per cup
+                    const savings_per_cup = (sb_price - 0.20).toFixed(2);
+                    p3.textContent += 'Having your coffee at home would save you $' + savings_per_cup + ' per cup! \r\n'
+                    const daily_savings = (qty*savings_per_cup).toFixed(2)
+                    p3.textContent += 'At $' + savings_per_cup + ' per cup and ' + qty + ' cups per day, that is $' + daily_savings + ' saved per day! \r\n'
+                    p3.textContent += '\r\nIf you instead invested that $' + daily_savings + ' per day:\r\n'
+                    var total_val = addCommas(calcTotalValue(55, parseInt(daily_savings)));
+                    p3.textContent += 'After 55 years at a 7% return, \r\n'
+                    p3.textContent += 'you would have $' + total_val + '!\r\n'
+                    p3.innerHTML += `<img style="margin:20px; width:500px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" src="woah.gif" alt="Bill Nye is amazed gif"/>`;
+                }           
+            });
+        })
+    }   
 }
 
-// format number 
+// format output number 
 
 function addCommas (number) {
     return number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
